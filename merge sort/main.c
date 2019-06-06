b787a7ea03e35ea506145cad2d9dbaeecbfa1833
HirushaR <hirusharandunu11@gmail.com>
2019-06-06 14:29:25 +0530

    add the merge sort

---
+++ merge sort/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void msort(int d[],int size){
+
+ int i,j,k, result[size], mid=size/2;
+
+ if(size==1)
+ {
+     return;
+ }
+
+
+ msort(d,mid);
+
+ msort(&d[mid],size-mid);
+
+ for(k=0,i=0,j=mid; i<mid && j<size; k++)
+    result[k]=d[i]<d[j]?d[i++]:d[j++];
+
+ while(i<mid)
+    result[k++]=d[i++];
+ while(j<size)
+  result[k++]=d[j++];
+
+ for(k=0;k<size;k++)
+ {
+     d[k]=result[k];
+ }
+
+}
+
+
+int main()
+{
+    int arr[7] = {5,3,7,2,1,6,4};
+    msort(arr,7);
+    int i;
+
+	for( i = 0 ; i<7 ; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+    return 0;
+}
